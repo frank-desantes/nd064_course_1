1243d4536c3dc46e7da9d38547e0ad3a4acdc326
Frank Pohlen <frank.pohlen@telekom.de>
2025-03-15 16:05:47 +0100

    add techtrends logfile

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -110,5 +110,5 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(name)s:%(message)s')
+   logging.basicConfig(filename='techtrends-debug.log',level=logging.DEBUG, format='%(levelname)s:%(name)s:%(message)s')
    app.run(host='0.0.0.0', port='3111')
